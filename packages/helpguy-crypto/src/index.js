dbc3057494b5c1b4e68950856fa39fcd0d15228e
alex <alex_my@126.com>
2019-12-21 17:05:48 +0800

    测试eslint

--- packages/helpguy-crypto/src/index.js
+++ packages/helpguy-crypto/src/index.js
@@ -1,13 +1,9 @@
 const crypto = require('crypto-js');
 
 class MyCrypto {
-  md5(str) {
-    console.log('hello md5 ...');
-    const a = {
-      a: 'bc1',
-    };
-    return crypto.MD5(str).toString();
-  }
+    md5(str) {
+        return crypto.MD5(str).toString();
+    }
 }
 
 export default MyCrypto;
