f18dfeca97d97c4781e06c6c7fe57270cf96a121
alex <alex_my@126.com>
2019-12-21 17:33:05 +0800

    测试eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,13 +4,15 @@ module.exports = {
         es6: true,
         node: true
     },
-    extends: ['airbnb-base'],
+    extends: ['airbnb-base', 'prettier', 'prettier/standard'],
+    plugins: ['prettier', 'standard'],
     parserOptions: {
         ecmaVersion: 2018,
         sourceType: 'module'
     },
     // https://cn.eslint.org/docs/rules/
     rules: {
+        'prettier/prettier': 'error',
         'class-methods-use-this': 'off'
     }
 };
