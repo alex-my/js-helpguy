1dc7ae778f9265457cafedf2095a563c92276c09
alex <alex_my@126.com>
2019-12-23 11:09:04 +0800

    更改测试文件所在的目录

--- packages/helpguy-crypto/test/index.test.js
+++ packages/helpguy-crypto/test/index.test.js
@@ -1,4 +1,4 @@
-const md5 = require('./index');
+const md5 = require('../src/index');
 
 test("md5('123456') to equal e10adc3949ba59abbe56e057f20f883e", () => {
     expect(md5('123456')).toBe('e10adc3949ba59abbe56e057f20f883e');
